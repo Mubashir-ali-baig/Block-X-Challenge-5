348055f26390d7e9f9443674c9219c7b3ca95ad2
Mubashir-ali-baig <mubashirbaig98@gmail.com>
2020-11-17 13:25:08 +0500

    release 1.0

--- TimeStamp.sol
+++ TimeStamp.sol
@@ -4,14 +4,14 @@ contract TimeStamp{
     address payable public beneficiary;
     uint256 public releaseTime;
     
-    constructor(address payable _beneficiary, uint256 _releaseTime) public{
+    constructor(address payable _beneficiary, uint256 _releaseTime)public payable{
         require(_releaseTime > block.timestamp);    
         beneficiary = _beneficiary;
         releaseTime = _releaseTime;
     }
     
-    function release() payable public{
+    function release() public{
         require(block.timestamp >= releaseTime);
-        address(beneficiary).transfer(address(this).balance);
+        beneficiary.transfer(address(this).balance);
     }
 }
